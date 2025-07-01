f422d5fe834466d806e57dbd752deff3de74f869
max <mrmaxwellxmiller@gmail.com>
2025-06-30 20:43:28 -0400

    added pausing and made code easier to read
    output still fits in 50kb

--- Pong/pong.cpp
+++ Pong/pong.cpp
@@ -5,13 +5,14 @@
 
 #define WIDTH 640
 #define HEIGHT 480
-#define FONT_SIZE HEIGHT/10
-#define SIZE WIDTH/40
-#define SPEED HEIGHT/50
+#define FONT_SIZE HEIGHT / 10
+#define SIZE WIDTH / 40
+#define SPEED HEIGHT / 50
 #define PI 3.14159265358979323846
 bool quit = false;
+bool pause = false;
 bool p2 = false;
-int frameCount, timerFPS, lastFrame, fps, scorel; // fix for garbage text on screen
+int frameCount, timerFPS, lastFrame, fps, scoreLength; // fix for garbage text on screen
 
 // Create window and renderer
 SDL_Window* window = SDL_CreateWindow("Pong", WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
@@ -24,30 +25,30 @@ TTF_Font* font;
 SDL_Color textColor = {255, 255, 255, 255}; // White text
 
 // Define destination rectangle
-SDL_FRect l_paddle, r_paddle, ball, score_board;
+SDL_FRect lPaddle, rPaddle, ball, scoreBoard;
 float velX, velY;
 std::string score;
-int l_s, r_s;
+int lScore, rScore;
 bool turn;
 
 void serve() {
-	l_paddle.y=r_paddle.y=(HEIGHT/2)-(l_paddle.h/2);
-	if(turn) {
-		ball.x=l_paddle.x+(l_paddle.w*4);
-		velX=SIZE/2;
+	lPaddle.y = rPaddle.y = (HEIGHT / 2) - (lPaddle.h / 2);
+	if (turn) {
+		ball.x = lPaddle.x + (lPaddle.w * 4);
+		velX = SIZE / 2;
 	}
 	else {
-		ball.x=r_paddle.x-(r_paddle.w*4);
-		velX=-SIZE/2;
+		ball.x = rPaddle.x - (rPaddle.w * 4);
+		velX = -SIZE / 2;
 	}
-	velY=0;
-	ball.y=HEIGHT/2-(SIZE/2);
-	turn=!turn;
+	velY = 0;
+	ball.y = HEIGHT / 2 - (SIZE / 2);
+	turn = !turn;
 }
 
-void write(std::string text, int x, int y) {    
+void write(std::string text, int textLength, int x, int y) {
 	const char* t = text.c_str();
-	SDL_Surface *surface = TTF_RenderText_Solid(font, t, scorel, textColor);
+	SDL_Surface *surface = TTF_RenderText_Solid(font, t, scoreLength, textColor);
 	if (surface == nullptr) {
 		std::cerr << "Failed to render text surface! TTF_Error: " << SDL_GetError() << std::endl;
 		TTF_CloseFont(font);
@@ -64,49 +65,74 @@ void write(std::string text, int x, int y) {
 		SDL_Quit();
 	}
 	
-	score_board.w=surface->w;
-	score_board.h=surface->h;
-	score_board.x=x-score_board.w;
-	score_board.y=y-score_board.h;
+	scoreBoard.w = surface -> w;
+	scoreBoard.h = surface -> h;
+	scoreBoard.x = x - scoreBoard.w;
+	scoreBoard.y = y - scoreBoard.h;
 	SDL_DestroySurface(surface); // Destroy the surface after creating the texture
-	SDL_RenderTexture(renderer, texture, nullptr, &score_board);
+	SDL_RenderTexture(renderer, texture, nullptr, &scoreBoard);
 	SDL_DestroyTexture(texture); // Destroy the texture
 }
 
 void update() {
-	if(SDL_HasRectIntersectionFloat(&ball, &r_paddle)) {
-		double rel = (r_paddle.y+(r_paddle.h/2))-(ball.y+(SIZE/2));
-		double norm = rel/(r_paddle.h/2);
-		double bounce = norm * (5*PI/12);
-		velX=-SIZE*cos(bounce);
-		velY=SIZE*-sin(bounce);
+	if (SDL_HasRectIntersectionFloat(&ball, &rPaddle)) {
+		double rel = (rPaddle.y + (rPaddle.h / 2)) - (ball.y + (SIZE / 2));
+		double norm = rel / (rPaddle.h / 2);
+		double bounce = norm * (5 * PI / 12);
+		velX= -SIZE * cos(bounce);
+		velY= SIZE * -sin(bounce);
 	}
-	if(SDL_HasRectIntersectionFloat(&ball, &l_paddle)) {
-		double rel = (l_paddle.y+(l_paddle.h/2))-(ball.y+(SIZE/2));
-		double norm = rel/(l_paddle.h/2);
-		double bounce = norm * (5*PI/12);
-		velX=SIZE*cos(bounce);
-		velY=SIZE*-sin(bounce);
+
+	if (SDL_HasRectIntersectionFloat(&ball, &lPaddle)) {
+		double rel = (lPaddle.y + (lPaddle.h / 2)) - (ball.y + (SIZE / 2));
+		double norm = rel / (lPaddle.h / 2);
+		double bounce = norm * (5 * PI / 12);
+		velX = SIZE * cos(bounce);
+		velY = SIZE * -sin(bounce);
 	}
+
 	if (!p2) {
-		if(ball.y > r_paddle.y+(r_paddle.h/2)) r_paddle.y+=SPEED;
-		if(ball.y < r_paddle.y+(r_paddle.h/2)) r_paddle.y-=SPEED;
+		if (ball.y > rPaddle.y + (rPaddle.h / 2)) {
+			rPaddle.y += SPEED;
+		}
+		if (ball.y < rPaddle.y + (rPaddle.h / 2)) {
+			rPaddle.y -= SPEED;
+		}
 	}
-	if(ball.x<=0) {
-		r_s++;serve();
+
+	if (ball.x <= 0) {
+		rScore++;
+		serve();
 	}
-	if(ball.x+SIZE>=WIDTH) {
-		l_s++;serve();
+	if (ball.x + SIZE >= WIDTH) {
+		lScore++;
+		serve();
 	}
-	if(ball.y<=0 || ball.y+SIZE>=HEIGHT) velY=-velY;
-	ball.x+=velX;
-	ball.y+=velY;
-	score=std::to_string(l_s) + "     " + std::to_string(r_s);
-	scorel=score.length();
-	if(l_paddle.y<0)l_paddle.y=0;
-	if(l_paddle.y+l_paddle.h>HEIGHT)l_paddle.y=HEIGHT-l_paddle.h;
-	if(r_paddle.y<0)r_paddle.y=0;
-	if(r_paddle.y+r_paddle.h>HEIGHT)r_paddle.y=HEIGHT-r_paddle.h;
+
+	if (ball.y <= 0 || ball.y + SIZE >= HEIGHT) {
+		velY = -velY;
+	}
+
+	ball.x += velX;
+	ball.y += velY;
+	score = std::to_string(lScore) + "     " + std::to_string(rScore);
+	scoreLength = score.length();
+
+	if (lPaddle.y < 0) {
+		lPaddle.y = 0;
+	}
+
+	if (lPaddle.y + lPaddle.h > HEIGHT) {
+		lPaddle.y = HEIGHT - lPaddle.h;
+	}
+
+	if (rPaddle.y < 0) {
+		rPaddle.y = 0;
+	}
+
+	if (rPaddle.y + rPaddle.h > HEIGHT) {
+		rPaddle.y = HEIGHT - rPaddle.h;
+	}
 }
 
 void input() {
@@ -117,18 +143,73 @@ void input() {
         }
     }
 	const bool *keystates = SDL_GetKeyboardState(NULL);
-	if(keystates[SDL_SCANCODE_ESCAPE]) quit=true;
-	if(keystates[SDL_SCANCODE_W]) l_paddle.y-=SPEED;
-	if(keystates[SDL_SCANCODE_S]) l_paddle.y+=SPEED;
-	if (p2) {
-		if(keystates[SDL_SCANCODE_2]) p2=false;
-		if(keystates[SDL_SCANCODE_UP]) r_paddle.y-=SPEED;
-		if(keystates[SDL_SCANCODE_DOWN]) r_paddle.y+=SPEED;
+	if (keystates[SDL_SCANCODE_ESCAPE]) {
+		quit = true;
+	}
+	if (keystates[SDL_SCANCODE_END]) {
+		quit = true;
+	}
+	if (pause) {
+		if (keystates[SDL_SCANCODE_RETURN]) {
+			pause = false;
+		}
+		if (keystates[SDL_SCANCODE_KP_ENTER]) {
+			pause = false;
+		}
+		if (keystates[SDL_SCANCODE_PAUSE]) {
+			pause = false;
+		}
 	}
 	else {
-		if(keystates[SDL_SCANCODE_2]) p2=true;
-		if(keystates[SDL_SCANCODE_UP]) l_paddle.y-=SPEED;
-		if(keystates[SDL_SCANCODE_DOWN]) l_paddle.y+=SPEED;
+		if (keystates[SDL_SCANCODE_RETURN]) {
+			pause = true;
+		}
+		if (keystates[SDL_SCANCODE_KP_ENTER]) {
+			pause = true;
+		}
+		if (keystates[SDL_SCANCODE_PAUSE]) {
+			pause = true;
+		}
+		if (keystates[SDL_SCANCODE_W]) {
+			lPaddle.y -= SPEED;
+		}
+		if (keystates[SDL_SCANCODE_S]) {
+			lPaddle.y += SPEED;
+		}
+		if (p2) {
+			if (keystates[SDL_SCANCODE_2]) {
+				p2 = false;
+			}
+			if (keystates[SDL_SCANCODE_UP]) {
+				rPaddle.y -= SPEED;
+			}
+			if (keystates[SDL_SCANCODE_DOWN]) {
+				rPaddle.y += SPEED;
+			}
+			if (keystates[SDL_SCANCODE_KP_8]) {
+				rPaddle.y -= SPEED;
+			}
+			if (keystates[SDL_SCANCODE_KP_2]) {
+				rPaddle.y += SPEED;
+			}
+		}
+		else {
+			if (keystates[SDL_SCANCODE_2]) {
+				p2 = true;
+			}
+			if (keystates[SDL_SCANCODE_UP]) {
+				lPaddle.y -= SPEED;
+			}
+			if (keystates[SDL_SCANCODE_DOWN]) {
+				lPaddle.y += SPEED;
+			}
+			if (keystates[SDL_SCANCODE_KP_8]) {
+				lPaddle.y -= SPEED;
+			}
+			if (keystates[SDL_SCANCODE_KP_2]) {
+				lPaddle.y += SPEED;
+			}
+		}
 	}
 }
 
@@ -137,9 +218,9 @@ void render() {
     SDL_RenderClear(renderer);
     
 	frameCount++;
-	timerFPS = SDL_GetTicks()-lastFrame;
-	if(timerFPS<(1000/60)) {
-		SDL_Delay((1000/60-timerFPS));
+	timerFPS = SDL_GetTicks() - lastFrame;
+	if (timerFPS < (1000 / 60)) {
+		SDL_Delay((1000 / 60 - timerFPS));
 	}
 	
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White objects
@@ -151,10 +232,10 @@ void render() {
 			SDL_RenderPoint(renderer, WIDTH / 2, y);
 		}
 	}
-	SDL_RenderFillRect(renderer, &l_paddle);
-	SDL_RenderFillRect(renderer, &r_paddle);
+	SDL_RenderFillRect(renderer, &lPaddle);
+	SDL_RenderFillRect(renderer, &rPaddle);
 	SDL_RenderFillRect(renderer, &ball);
-	write(score, WIDTH/2+FONT_SIZE*3.5, FONT_SIZE*2);
+	write(score, scoreLength, WIDTH / 2 + FONT_SIZE * 3.5, FONT_SIZE * 2);
 	SDL_RenderPresent(renderer);
 }
 
@@ -175,24 +256,26 @@ int main(int argc, char* argv[]) {
 	font = TTF_OpenFont("../PressStart2P-Regular.ttf", FONT_SIZE);
     static int lastTime = 0;
     
-    l_s=r_s=0;
-    l_paddle.x=32; l_paddle.h=HEIGHT/4;
-    l_paddle.y=(HEIGHT/2)-(l_paddle.h/2);
-    l_paddle.w=SIZE;
-    r_paddle=l_paddle;
-    r_paddle.x=WIDTH-r_paddle.w-32;
-    ball.w=ball.h=SIZE;
+    lScore = rScore = 0;
+    lPaddle.x = 32; lPaddle.h = HEIGHT / 4;
+    lPaddle.y = (HEIGHT / 2) - (lPaddle.h / 2);
+    lPaddle.w = SIZE;
+    rPaddle = lPaddle;
+    rPaddle.x = WIDTH - rPaddle.w - 32;
+    ball.w = ball.h = SIZE;
     
     serve();
     
     while (!quit) {
-    	lastFrame=SDL_GetTicks();
-    	if(lastFrame>=(lastTime+1000)) {
-    		lastTime=lastFrame;
-    		fps=frameCount;
-    		frameCount=0;
+    	lastFrame = SDL_GetTicks();
+    	if (lastFrame >= (lastTime + 1000)) {
+    		lastTime = lastFrame;
+    		fps = frameCount;
+    		frameCount = 0;
     	}
-		update();
+    	if (!pause) {
+			update();
+		}
 		input();
 		render();
     }
